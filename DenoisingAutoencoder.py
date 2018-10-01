1f0c02449b5f429888650e6e7c8f76d03ceb662b
Pranav Vempati <pranav.k.vempati@gmail.com>
2018-09-30 17:57:52 -0700

    Add files via upload

--- DenoisingAutoencoder.py
+++ DenoisingAutoencoder.py
@@ -3,10 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from tensorflow.examples.tutorials.mnist import input_data
 
-'''
-A denoising autoencoder that reconstructs MNIST digits
-'''
-
 num_displayed_images = 5
 
 mnist = input_data.read_data_sets("MNIST_data", one_hot = True)
@@ -25,58 +21,89 @@ num_hidden5_units = num_hidden1_units
 
 num_output_units = num_visible_units
 
-visible_placeholder = tf.placeholder(tf.float32, shape = [None, num_visible_units]) # Define a placeholder for the input
+visible_placeholder = tf.placeholder(tf.float32, shape = [None, num_visible_units]) # Define a placeholder for the MNIST images 
 
-w1 = tf.Variable(tf.truncated_normal([num_visible_units,num_hidden1_units]), dtype = tf.float32) # Layerwise initialization of the network's trainable weight matrices
-w2 = tf.Variable(tf.truncated_normal([num_hidden1_units, num_hidden2_units]), dtype = tf.float32)
-w3 = tf.Variable(tf.truncated_normal([num_hidden2_units, num_hidden3_units]), dtype = tf.float32)
-w4 = tf.Variable(tf.truncated_normal([num_hidden3_units, num_hidden4_units]), dtype = tf.float32)
-w5 = tf.Variable(tf.truncated_normal([num_hidden4_units, num_hidden5_units]), dtype = tf.float32)
-w6 = tf.Variable(tf.truncated_normal([num_hidden5_units, num_output_units]), dtype = tf.float32)
+def weight_matrix(input_dim, output_dim):
+	return tf.Variable(tf.truncated_normal([input_dim, output_dim]), dtype = tf.float32)
+
+def bias_vector(output_dim):
+	return tf.Variable(tf.zeros([output_dim]))
 
 
-b1 = tf.Variable(tf.zeros(num_hidden1_units)) # Layerwise initialization of the network's trainable bias vectors
-b2 = tf.Variable(tf.zeros(num_hidden2_units))
-b3 = tf.Variable(tf.zeros(num_hidden3_units))
-b4 = tf.Variable(tf.zeros(num_hidden4_units))
-b5 = tf.Variable(tf.zeros(num_hidden5_units))
-b6 = tf.Variable(tf.zeros(num_output_units))
+def log_tensor(tensor):
+	with tf.name_scope('consolidated_summaries'):
+		mean = tf.reduce_mean(tensor)
+		tf.summary.scalar('mean', mean)
+	with tf.name_scope('standard_deviation'):
+		standard_deviation = tf.sqrt(tf.reduce_mean(tf.square(tensor-mean)))
+	tf.summary.scalar('standard_deviation', standard_deviation)
+	tf.summary.histogram('histogram', tensor)
 
+def network_layer(incoming_tensor, input_dim, output_dim, layer_name):
+	with tf.name_scope(layer_name):
+		with tf.name_scope("Weights"):
+			weights = weight_matrix(input_dim, output_dim)
+			log_tensor(weights)
+		with tf.name_scope("Biases"):
+			biases = bias_vector(output_dim)
+			log_tensor(biases)
+		activations = tf.nn.relu(tf.matmul(incoming_tensor, weights) + biases)
+		tf.summary.histogram('activations', activations)
+		return activations
+
+def corrupt_inputs(pristine_inputs, additive_noise):
+	return pristine_inputs + additive_noise
 
 gaussian_noise = tf.random_normal([num_visible_units], mean = 0.0, stddev = 1.0, dtype = tf.float32)
-corrupted_layer = visible_placeholder + gaussian_noise # Corrupt inputs with Gaussian additive noise
-hidden_layer_1_activations = tf.nn.relu(tf.matmul(corrupted_layer, w1) + b1)
-hidden_layer_2_activations = tf.nn.relu(tf.matmul(hidden_layer_1_activations, w2) + b2)
-hidden_layer_3_activations = tf.nn.relu(tf.matmul(hidden_layer_2_activations, w3) + b3)
-hidden_layer_4_activations = tf.nn.relu(tf.matmul(hidden_layer_3_activations, w4) + b4)
-hidden_layer_5_activations = tf.nn.relu(tf.matmul(hidden_layer_4_activations, w5) + b5)
-output = tf.nn.relu(tf.matmul(hidden_layer_5_activations, w6) + b6)
 
-mse_loss = tf.reduce_mean(tf.square(output - visible_placeholder)) # MSE(mean-squared-error) reconstruction loss between the autoencoder's reconstructions and inputs. Cross entropy loss tends to be asymmetric, which assigns erroneous additional penalty to some reconstructions
+corrupted_inputs = corrupt_inputs(visible_placeholder, gaussian_noise)
 
-optimizer = tf.train.AdamOptimizer(0.01)
+first_layer = network_layer(corrupted_inputs, num_visible_units, num_hidden1_units, "First_Layer")
+second_layer = network_layer(first_layer, num_hidden1_units, num_hidden2_units, "Second_Layer")
+third_layer = network_layer(second_layer, num_hidden2_units, num_hidden3_units, "Third_Layer")
+fourth_layer = network_layer(third_layer, num_hidden3_units, num_hidden4_units, "Fourth_Layer")
+fifth_layer = network_layer(fourth_layer, num_hidden4_units, num_hidden5_units, "Fifth_Layer")
+outputs = network_layer(fifth_layer, num_hidden5_units, num_output_units, "Output_Layer")
 
-training = optimizer.minimize(mse_loss)
+with tf.name_scope("MSE_Loss"):
+	mse_loss = tf.reduce_mean(tf.square(outputs-visible_placeholder))
+tf.summary.scalar('MSE_Loss', mse_loss)
 
-num_epochs = 20
+with tf.name_scope("training"):
+	trainer = tf.train.AdamOptimizer(0.01).minimize(mse_loss)
+
+merged = tf.summary.merge_all()
+
+num_epochs = 12
 
 batch_size = 100
 
 num_examples = mnist.train.num_examples
+
+num_batches = int(num_examples//batch_size)
+
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
+	event_writer = tf.summary.FileWriter("./TensorBoardLogs", graph = sess.graph)
 	for epoch in range(num_epochs):
-		for batch in range(int(num_examples//batch_size)):
+		aggregate_batch_loss = 0
+		for batch in range(num_batches):
 			batch_examples, batch_targets = mnist.train.next_batch(batch_size)
-			sess.run(training, feed_dict = {visible_placeholder:batch_examples})
-		epochwise_loss = sess.run(mse_loss, feed_dict = {visible_placeholder:batch_examples})
-		print("Epoch: " + str(epoch + 1) + " Loss: " + str(epochwise_loss)) 
-	reconstructions = sess.run(output, feed_dict = {visible_placeholder:mnist.test.images[:num_displayed_images]})
+			batch_summary, _, batch_loss = sess.run([merged,trainer,mse_loss], feed_dict = {visible_placeholder:batch_examples})
+			aggregate_batch_loss += batch_loss
+			event_writer.add_summary(batch_summary, num_batches*epoch + batch)
+		epochwise_loss = (aggregate_batch_loss/num_batches)
+		print("Epoch: " + str(epoch + 1) + " Loss: " + str(epochwise_loss)) # Log loss to stdout at every epoch
+	training_summary,reconstructions = sess.run([merged,outputs], feed_dict = {visible_placeholder:mnist.test.images[:num_displayed_images]})
+
 
 	a, b = plt.subplots(2,10, figsize = (20,4))
 	for index in range(num_displayed_images):
 		b[0][index].imshow(np.reshape(mnist.test.images[index],(28,28)))
 		b[1][index].imshow(np.reshape(reconstructions[index],(28,28)))
+
+
+
 
 
 
